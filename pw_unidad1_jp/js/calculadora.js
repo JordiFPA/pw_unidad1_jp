3104a347018b322496b802de85779bd783890777
JordiFPA <167142190+JordiFPA@users.noreply.github.com>
2025-04-25 19:45:09 -0500

    Taller 8 DESESTRUCTURACIÓN DE ARREGLOS Y OBJETOS.

--- pw_unidad1_jp/js/calculadora.js
+++ pw_unidad1_jp/js/calculadora.js
@@ -97,42 +97,198 @@ const fundamentos = () => {
     }
 
     console.log(miProfesor);
-    console.log(miProfesor.nombre); 
-    /*Seteo de atributos*/ 
+    console.log(miProfesor.nombre);
 
-    miProfesor.apellido = 'Aguaisa'; 
-    console.log(miProfesor); 
+    /*Seteo de atributos*/
 
-    if(miProfesor.ciudad ==='Quito') { 
+    miProfesor.apellido = 'Aguaisa';
+    console.log(miProfesor);
+
+    if (miProfesor.ciudad === 'Quito') {
         console.log('Es de la capital')
-    } else { 
+    } else {
         console.log('Pertenece a otra ciudad')
     }
-    const estudiante1 ={
+    const estudiante1 = {
         nombre: 'Fatima',
         apellido: 'Fiallos',
         edad: '23'
     }
 
-    const estudiante2 ={
+    const estudiante2 = {
         nombre: 'Bruno',
         apellido: 'Miranda',
         edad: '23'
     }
-    const estudiante3 ={
+    const estudiante3 = {
         nombre: 'Henry',
         apellido: 'Asd',
         edad: '23'
     }
 
 
-    const estudiantes = [estudiante1,estudiante2,estudiante3]; 
+    const estudiantes = [estudiante1, estudiante2, estudiante3];
 
-    console.table(estudiantes) 
+    console.table(estudiantes)
+
+    /*Referenciar atributo tipo objeto */
+
+    const ciudadano = {
+        nombre: 'Jordi',
+        apellido: 'Pila',
+        direccion: {
+            callePrincipal: 'Av. America',
+            calleSecundaria: 'Av. La Gasca',
+            numeracion: '5620',
+            barrio: {
+                referencia: 'Frente al Dilipa'
+            }
+        }
+    }
+
+    console.log(ciudadano);
+    console.log(ciudadano.nombre);
+    console.log(ciudadano.direccion)
+    console.log(ciudadano.direccion.callePrincipal)
+    ciudadano.direccion.callePrincipal = 'Av amazonas';
+    console.log(ciudadano);
+
+    /* Arreglo de varios estudiantes, pero diferentes, es decir combinando*/
+
+    const est1 = {
+        nombre: 'Edison'
+    }
+
+    const est2 = {
+        nombre: 'Daniel'
+    }
+
+    const arregloEstudiantes = [est1, est2];
+    console.log(arregloEstudiantes);
+
+    /*Arreglo de varios estudiantes, en una sola linea*/
+    const arregloEstudiantes2 = [{ nombre: 'Carla', apellido: 'Castillos' },
+    { nombre: 'Melany', apellido: 'Caizapasto' }]
+    console.log(arregloEstudiantes2);
+
+    console.log(arregloEstudiantes2[1].apellido);
+
+    //Desestructuracion de arreglos: 
+    const colores = ['Amarillo', 'Azul', 'Rojo', 'Verde', 'Naranja'];
+    const [c1, c2, c3, c4, c5] = colores;
+    console.log(c1);
+    console.log(c5);
+    const [cuno, cdos] = colores;
+    console.log(cuno);
+    console.log(cdos);
+    const [, , ctres] = colores;
+    console.log(ctres);
+
+    const [c01, c02] = ['Amarillo', 'Azul', 'Rojo', 'Verde', 'Naranja'];
+    console.log(c01);
+    console.log(c02);
+    desestructuracionArreglo(colores);
+    const [, p2, p3, p4, p5, p6, p7, p8, p9, p10] = desestructuracionArreglo2();
+    console.log(p2);
+    console.log(p3);
+    console.log(p10);
+
+    //Desestructuracion por operador REST
+    console.log('Desestructuracion por operador REST');
+    const [p1, ...resto] = desestructuracionArreglo2();
+    console.log(p1);
+    console.log(resto);
 
 
 
+    // Desestructuracion de objetos
+    const auto = {
+        marca: 'Toyota',
+        modelo: 'prius',
+        anio: 2020,
+        color: 'Amarillo'
+    }
+
+
+    const { marca, color, anio } = auto;
+    console.log(color);
+
+    const { anio1 } = {
+        marca1: 'Toyota',
+        modelo1: 'prius',
+        anio1: 2020,
+        color1: 'Amarillo'
+    }
+    console.log(anio1);
+
+    desestructuracionObjeto(auto);
+
+    const universidad = {
+        nombre: 'UCE',
+        direccion: 'America',
+        rector: {
+            nombreR: 'Patricio',
+            apellidoR: 'Espinoza',
+        }
+    }
+    //Desestructurar para  llegar al atribto de mi objeto dentro del atributo
+    const { rector, nombre } = universidad;
+    console.log(rector);
+    const { apellidoR } = rector;
+    console.log(apellidoR);
+    //Desestructuracion en una sola linea de un objeto que tiene atributo de tipo objeto: 
+
+    const universidad2 = {
+        nombre2: 'UCE',
+        direccion2: 'America',
+        rector2: {
+            nombreR2: 'Patricio2',
+            apellidoR2: 'Espinoza2',
+        }
+    }
+    const { nombre2, rector2: { nombreR2 } } = universidad2;
+    console.log(nombreR2)
+    console.log('Desestructuracion por operador REST para objetos');
+    //Desestructuracion por operador REST para objetos
+    const autoR = {
+        marcaR: 'Toyota',
+        modeloR: 'prius',
+        anioR: 2020,
+        colorR: 'Amarillo'
+    }
+
+    const{marcaR,...restoO} = autoR; 
+
+    console.log(marcaR); 
+    console.log(restoO); 
 }
+
+
+
+
+function desestructuracionArreglo([c1, c2, c3]) {
+    console.log(c1);
+    console.log(c2);
+    console.log(c3);
+}
+
+function desestructuracionArreglo2() {
+    const colores = ['Rosado', 'Blanco', 'Rojo', 'Verde', 'Naranja'];
+    return colores;
+}
+
+function desestructuracionObjeto({ marca, color }) {
+    console.log(marca);
+    console.log(color);
+}
+
+
+
+
+
+
+
+
 
 
 
